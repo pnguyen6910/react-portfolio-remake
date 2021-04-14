439d014d66752518e4ff6546e550aa5c540a7dab
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-04-14 02:34:53 -0500

    home page

---
+++ src/Pages/home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const About = () => (
+    <div className="container">
+        <div className="row">
+            <div className="container">
+                <section id="main-bio">
+                    <h2>Paul Nguyen</h2>
+                    <hr />
+                    <div className="row">
+                        <div className="col-lg-3">
+                            <img className="image-fluid" src="paul2.JPG" alt="Bio" id="bio-image" />
+                        </div>
+                        <div className="col-lg-9">
+                            <div className="bio-text">
+                                <p>My name is Paul Nguyen and I build things on the internet.
+
+As it says above, I'm based out of Chicago. I'm a young passionate developer using his knowledge in the arts and applying it to web development. Creating websites was a newfound passion that I didn't even know I was going to enjoy, but nonetheless I absolutely love it.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+)
+
+export default About
